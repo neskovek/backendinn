510495e5460a3dfeb03a4a6d9b31f1cf4bd430d1
gus <dev@ninsaude.com>
2026-06-17 13:09:57 -0300

    testes arrumados

--- src/main.ts
+++ src/main.ts
@@ -26,7 +26,9 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-  await app.listen(process.env.PORT ?? 5000, '0.0.0.0');
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
+
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 
 bootstrap().catch((err) => {
